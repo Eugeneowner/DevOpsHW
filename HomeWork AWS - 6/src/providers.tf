a65077d759795727d99b815f3a34d77009d8a638
Eugene <129581606+Eugeneowner@users.noreply.github.com>
2025-04-20 07:56:23 +0300

    Update providers.tf
    add readme

--- HomeWork AWS - 6/src/providers.tf
+++ HomeWork AWS - 6/src/providers.tf
@@ -3,11 +3,3 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-danit-devops-7"
-    key     = "rzayats/terraform.tfstate"
-    region  = "eu-central-1"
-    profile = "mfa"
-  }
-}
