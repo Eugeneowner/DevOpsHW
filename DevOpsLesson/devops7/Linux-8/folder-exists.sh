949c6a4d3188dd11e60fbe729c4e98c456370366
Eugeneowner <ownereugene@gmail.com>
2025-06-29 21:42:10 +0300

    Add home work 27

--- DevOpsLesson/devops7/Linux-8/folder-exists.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -n "Enter folder path: "; read dir_path
-
-if [ -d "${dir_path}" ]; then
-	echo "The folder ${dir_path} is exists"
-else
-	echo "The folder ${dir_path} does not exist!"
-fi
--- DevOpsLesson/devops7/Linux-9/disk_space_logger.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Script executed at $(date)" >> "/var/log/disk_space_$(date '+%Y-%m-%d_%H-%M-%S').log"
-df -h >> "/var/log/disk_space_$(date '+%Y-%m-%d_%H-%M-%S').log"
